090513967b83e74a18239cdea179c7d382b79bc4
UzumiKajitsu <minh75606@gmail.com>
2024-10-14 09:15:43 +0700

    fix security group, update readme for terraform

--- Lab1/terraform/modules/security-group/main.tf
+++ Lab1/terraform/modules/security-group/main.tf
@@ -8,6 +8,15 @@ resource "aws_security_group" "public_security_group" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
   tags = {
     name = "Public_security_group"
@@ -24,6 +33,14 @@ resource "aws_security_group" "private_security_group" {
     to_port     = 22
     protocol    = "tcp"
     security_groups = [aws_security_group.public_security_group.id]
+  }
+
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
   tags = {
     name = "Private_security_group"
